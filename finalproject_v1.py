c94f1e018c3ae4c9d7832889350ae6751490150d
prem-pa <64376689+prem-pa@users.noreply.github.com>
2020-04-26 15:02:07 -0400

    Add files via upload

---
+++ finalproject_v1.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 20 16:45:45 2020
+
+@author: prem
+"""
+
+# api_key = zk9hWP522GvyDxfggGgNGdIyEoQZPRl2Teaj48SvsZ9ciUAyCN1YwdR1jbnY
+# api_key1 = 5XBMT53K2J7WAHGQ
+
+#import requests
+#import csv
+import os
+import datetime
+import pandas as pd
+#import csv
+
+print(os.getcwd())
+datetoday = datetime.datetime.today().strftime('%Y-%m-%d')
+
+csv_url_confirmed_cases = 'https://data.humdata.org/hxlproxy/data/download/time_series_covid19_confirmed_global_narrow.csv?dest=data_edit&filter01=merge&merge-url01=https%3A%2F%2Fdocs.google.com%2Fspreadsheets%2Fd%2Fe%2F2PACX-1vTglKQRXpkKSErDiWG6ycqEth32MY0reMuVGhaslImLjfuLU0EUgyyu2e-3vKDArjqGX7dXEBV8FJ4f%2Fpub%3Fgid%3D1326629740%26single%3Dtrue%26output%3Dcsv&merge-keys01=%23country%2Bname&merge-tags01=%23country%2Bcode%2C%23region%2Bmain%2Bcode%2C%23region%2Bsub%2Bcode%2C%23region%2Bintermediate%2Bcode&filter02=merge&merge-url02=https%3A%2F%2Fdocs.google.com%2Fspreadsheets%2Fd%2Fe%2F2PACX-1vTglKQRXpkKSErDiWG6ycqEth32MY0reMuVGhaslImLjfuLU0EUgyyu2e-3vKDArjqGX7dXEBV8FJ4f%2Fpub%3Fgid%3D398158223%26single%3Dtrue%26output%3Dcsv&merge-keys02=%23adm1%2Bname&merge-tags02=%23country%2Bcode%2C%23region%2Bmain%2Bcode%2C%23region%2Bsub%2Bcode%2C%23region%2Bintermediate%2Bcode&merge-replace02=on&merge-overwrite02=on&filter03=explode&explode-header-att03=date&explode-value-att03=value&filter04=rename&rename-oldtag04=%23affected%2Bdate&rename-newtag04=%23date&rename-header04=Date&filter05=rename&rename-oldtag05=%23affected%2Bvalue&rename-newtag05=%23affected%2Binfected%2Bvalue%2Bnum&rename-header05=Value&filter06=clean&clean-date-tags06=%23date&filter07=sort&sort-tags07=%23date&sort-reverse07=on&filter08=sort&sort-tags08=%23country%2Bname%2C%23adm1%2Bname&tagger-match-all=on&tagger-default-tag=%23affected%2Blabel&tagger-01-header=province%2Fstate&tagger-01-tag=%23adm1%2Bname&tagger-02-header=country%2Fregion&tagger-02-tag=%23country%2Bname&tagger-03-header=lat&tagger-03-tag=%23geo%2Blat&tagger-04-header=long&tagger-04-tag=%23geo%2Blon&header-row=1&url=https%3A%2F%2Fraw.githubusercontent.com%2FCSSEGISandData%2FCOVID-19%2Fmaster%2Fcsse_covid_19_data%2Fcsse_covid_19_time_series%2Ftime_series_covid19_confirmed_global.csv'
+csv_url_recovered_cases = 'https://data.humdata.org/hxlproxy/data/download/time_series_covid19_recovered_global_narrow.csv?dest=data_edit&filter01=merge&merge-url01=https%3A%2F%2Fdocs.google.com%2Fspreadsheets%2Fd%2Fe%2F2PACX-1vTglKQRXpkKSErDiWG6ycqEth32MY0reMuVGhaslImLjfuLU0EUgyyu2e-3vKDArjqGX7dXEBV8FJ4f%2Fpub%3Fgid%3D1326629740%26single%3Dtrue%26output%3Dcsv&merge-keys01=%23country%2Bname&merge-tags01=%23country%2Bcode%2C%23region%2Bmain%2Bcode%2C%23region%2Bsub%2Bcode%2C%23region%2Bintermediate%2Bcode&filter02=merge&merge-url02=https%3A%2F%2Fdocs.google.com%2Fspreadsheets%2Fd%2Fe%2F2PACX-1vTglKQRXpkKSErDiWG6ycqEth32MY0reMuVGhaslImLjfuLU0EUgyyu2e-3vKDArjqGX7dXEBV8FJ4f%2Fpub%3Fgid%3D398158223%26single%3Dtrue%26output%3Dcsv&merge-keys02=%23adm1%2Bname&merge-tags02=%23country%2Bcode%2C%23region%2Bmain%2Bcode%2C%23region%2Bsub%2Bcode%2C%23region%2Bintermediate%2Bcode&merge-replace02=on&merge-overwrite02=on&filter03=explode&explode-header-att03=date&explode-value-att03=value&filter04=rename&rename-oldtag04=%23affected%2Bdate&rename-newtag04=%23date&rename-header04=Date&filter05=rename&rename-oldtag05=%23affected%2Bvalue&rename-newtag05=%23affected%2Binfected%2Bvalue%2Bnum&rename-header05=Value&filter06=clean&clean-date-tags06=%23date&filter07=sort&sort-tags07=%23date&sort-reverse07=on&filter08=sort&sort-tags08=%23country%2Bname%2C%23adm1%2Bname&tagger-match-all=on&tagger-default-tag=%23affected%2Blabel&tagger-01-header=province%2Fstate&tagger-01-tag=%23adm1%2Bname&tagger-02-header=country%2Fregion&tagger-02-tag=%23country%2Bname&tagger-03-header=lat&tagger-03-tag=%23geo%2Blat&tagger-04-header=long&tagger-04-tag=%23geo%2Blon&header-row=1&url=https%3A%2F%2Fraw.githubusercontent.com%2FCSSEGISandData%2FCOVID-19%2Fmaster%2Fcsse_covid_19_data%2Fcsse_covid_19_time_series%2Ftime_series_covid19_recovered_global.csv'
+csv_url_deaths = 'https://data.humdata.org/hxlproxy/data/download/time_series_covid19_deaths_global_narrow.csv?dest=data_edit&filter01=merge&merge-url01=https%3A%2F%2Fdocs.google.com%2Fspreadsheets%2Fd%2Fe%2F2PACX-1vTglKQRXpkKSErDiWG6ycqEth32MY0reMuVGhaslImLjfuLU0EUgyyu2e-3vKDArjqGX7dXEBV8FJ4f%2Fpub%3Fgid%3D1326629740%26single%3Dtrue%26output%3Dcsv&merge-keys01=%23country%2Bname&merge-tags01=%23country%2Bcode%2C%23region%2Bmain%2Bcode%2C%23region%2Bsub%2Bcode%2C%23region%2Bintermediate%2Bcode&filter02=merge&merge-url02=https%3A%2F%2Fdocs.google.com%2Fspreadsheets%2Fd%2Fe%2F2PACX-1vTglKQRXpkKSErDiWG6ycqEth32MY0reMuVGhaslImLjfuLU0EUgyyu2e-3vKDArjqGX7dXEBV8FJ4f%2Fpub%3Fgid%3D398158223%26single%3Dtrue%26output%3Dcsv&merge-keys02=%23adm1%2Bname&merge-tags02=%23country%2Bcode%2C%23region%2Bmain%2Bcode%2C%23region%2Bsub%2Bcode%2C%23region%2Bintermediate%2Bcode&merge-replace02=on&merge-overwrite02=on&filter03=explode&explode-header-att03=date&explode-value-att03=value&filter04=rename&rename-oldtag04=%23affected%2Bdate&rename-newtag04=%23date&rename-header04=Date&filter05=rename&rename-oldtag05=%23affected%2Bvalue&rename-newtag05=%23affected%2Binfected%2Bvalue%2Bnum&rename-header05=Value&filter06=clean&clean-date-tags06=%23date&filter07=sort&sort-tags07=%23date&sort-reverse07=on&filter08=sort&sort-tags08=%23country%2Bname%2C%23adm1%2Bname&tagger-match-all=on&tagger-default-tag=%23affected%2Blabel&tagger-01-header=province%2Fstate&tagger-01-tag=%23adm1%2Bname&tagger-02-header=country%2Fregion&tagger-02-tag=%23country%2Bname&tagger-03-header=lat&tagger-03-tag=%23geo%2Blat&tagger-04-header=long&tagger-04-tag=%23geo%2Blon&header-row=1&url=https%3A%2F%2Fraw.githubusercontent.com%2FCSSEGISandData%2FCOVID-19%2Fmaster%2Fcsse_covid_19_data%2Fcsse_covid_19_time_series%2Ftime_series_covid19_deaths_global.csv'
+
+#The following three lines of code will take time to execute. Run them individually
+confirmed_cases = pd.read_csv(csv_url_confirmed_cases)
+recoved_cases = pd.read_csv(csv_url_recovered_cases)
+deaths = pd.read_csv(csv_url_deaths)
+
+#Saving the csv files locally
+confirmed_cases.to_csv('G:/My Drive/Semester 2/CPE 695/Final Project/Data files/confirmed_cases_' + datetoday + '.csv', index=False, line_terminator='\n')
+recoved_cases.to_csv('G:/My Drive/Semester 2/CPE 695/Final Project/Data files/recoved_cases_' + datetoday + '.csv', index=False, line_terminator='\n')
+deaths.to_csv('G:/My Drive/Semester 2/CPE 695/Final Project/Data files/deaths_' + datetoday + '.csv', index=False, line_terminator='\n')
+
+#Data cleaning
+confirmed_cases = confirmed_cases[confirmed_cases['Country/Region'] == 'US']
+recoved_cases = recoved_cases[recoved_cases['Country/Region'] == 'US']
+deaths = deaths[deaths['Country/Region'] == 'US']
+
+confirmed_cases = confirmed_cases.filter(['Date', 'Value'])
+recoved_cases = recoved_cases.filter(['Date', 'Value'])
+deaths = deaths.filter(['Date', 'Value'])
+
+#Renaming columns
+confirmed_cases = confirmed_cases.rename({"Value":"Confirmed_cases"}, axis='columns')
+recoved_cases = recoved_cases.rename({"Value":"Recoved_cases"}, axis='columns')
+deaths = deaths.rename({"Value":"Deaths"}, axis='columns')
+
+#Data Merging (COVID)
+covid_data = pd.merge(confirmed_cases, recoved_cases, on=['Date'], how='outer')
+covid_data_final = pd.merge(covid_data, deaths, on=['Date'], how='outer')
+
+#Stock market data
+import yfinance as yf
+#test = yf.download('AAP','2018-01-01','2020-04-04')
+
+#Loading compnay index data
+company_data = pd.read_csv('G:/My Drive/Semester 2/CPE 695/Final Project/Data files/constituents_csv.csv')
+c_symbol = company_data['Symbol'].values.tolist()
+
+#Merging all stock market data into one data frame
+data = []
+final_data = []
+final_data = pd.DataFrame(final_data, columns = ['Date', 'Company', 'Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume']) 
+
+for i in range(len(c_symbol)):
+    
+    #Download stock data
+    data = yf.download(c_symbol[i],'2018-01-01',datetoday)
+    data = data.reset_index()
+    
+    if i == 0:
+        final_data = data
+    else:
+        final_data = pd.merge(final_data, data, on=['Date'], how='outer')
+    
+    #Renaming the columns
+    final_data = final_data.rename({"Company":"Company_" + c_symbol[i], "Open":"Open_" + c_symbol[i],"High":"High_" + c_symbol[i],"Low":"Low_" + c_symbol[i],"Close":"Close_" + c_symbol[i],"Adj Close":"Adj Close_" + c_symbol[i],"Volume":"Volume_" + c_symbol[i]}, axis='columns')
